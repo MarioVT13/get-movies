911851c06acb1c028c82dbd57384e4a213467779
Mario Vatkov <mariovt13@gmail.com>
2024-05-10 21:41:01 +0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -10,11 +11,13 @@ export default function App() {
   );
 }
 
+export default gestureHandlerRootHOC(App);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
